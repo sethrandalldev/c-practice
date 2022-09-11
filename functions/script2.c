de1436e48e90c4586d2d1384152298dc6f99bd76
Seth Randall <sethjrandall@gmail.com>
2022-09-10 20:32:05 -0400

    Finish lesson5 in functions

---
+++ functions/script2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(void)
+{
+  srand(time(NULL));
+  // Write your code below
+  int randomNumber = rand() % 20 + 1;
+  printf("%d\n", randomNumber);
+}
---
+++ functions/script3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// Write your code below
+void printMyFavoriteAnimal(void)
+{
+  printf("My favorite animal is a gorilla\n");
+}
+
+int main(void)
+{
+  printMyFavoriteAnimal();
+}
